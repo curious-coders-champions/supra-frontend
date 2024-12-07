d16628b4050cc1e13c18a841a30c1f31f44740ff
chann44 <44chansw@gmail.com>
2024-12-08 04:53:05 +0530

    feat: description added

--- app/[market_id]/page.tsx
+++ app/[market_id]/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { LineChart } from "@/components/charts/LineChart";
 import { AboutMarket } from "@/components/market-details/about-market";
 import { MarketHeader } from "@/components/market-details/market-header";
@@ -33,7 +34,7 @@ export default function MarketDetailsPage() {
             }} />
             <LineChart />
             <MarketStasts />
-            <AboutMarket name="Wraped ETher" description="orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum" />
+            <AboutMarket name="Wraped ETher" description="$SUPRA powers the Supra blockchain, enabling transactions, staking, and governance. It supports DeFi apps, cross-chain interoperability, and scalability, fostering innovation in the decentralized ecosystem." />
         </section>
         <Card className="col-span-2 bg-transparent h-fit shadow-none border-none -mt-6 pb-4">
             <SwapInterface />
