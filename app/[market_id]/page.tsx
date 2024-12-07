736907e1b72d649843c88240bcfef09b1d46e206
KetanKudikyal <krotusppp@gmail.com>
2024-12-08 02:43:46 +0530

    feat: ✨ add mint interface to market details page

--- app/[market_id]/page.tsx
+++ app/[market_id]/page.tsx
@@ -3,7 +3,7 @@ import { LineChart } from "@/components/charts/LineChart";
 import { AboutMarket } from "@/components/market-details/about-market";
 import { MarketHeader } from "@/components/market-details/market-header";
 import { MarketStasts } from "@/components/market-details/market-stats";
-import { MarketSwapTabs } from "@/components/swap/swap-tabs";
+import MintInterface from "@/components/mint-interface";
 import { Card } from "@/components/ui/card";
 import usePrice from "@/hooks/usePrice";
 
@@ -34,8 +34,8 @@ export default function MarketDetailsPage() {
             <MarketStasts />
             <AboutMarket name="Wraped ETher" description="orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum" />
         </section>
-        <Card className="col-span-2 h-fit pb-4">
-            <MarketSwapTabs />
+        <Card className="col-span-2 bg-transparent h-fit shadow-none border-none -mt-6 pb-4">
+            <MintInterface />
         </Card>
     </main>
 }
