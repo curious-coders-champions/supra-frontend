76095a40504c379cb02b07a8b31d492d963a797c
KetanKudikyal <krotusppp@gmail.com>
2024-12-08 04:08:41 +0530

    feat: 🎨 update navbar styling and add logo image

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,21 +1,24 @@
 'use client'
 
+import { useAccount } from "@/hooks/useConnectWallet"
+import Image from "next/image"
 import Link from "next/link"
 import { ConnectWalletButton } from "../connect-wallet"
-import { useAccount } from "@/hooks/useConnectWallet"
-import UserProfileNav from "../user-profile"
 import { HistoryModal } from "../history"
+import UserProfileNav from "../user-profile"
 
 const Navbar = () => {
     const { isConnected } = useAccount()
-
     return (
         <div className='h-[80px] w-full'>
             <div className="max-w-7xl flex items-center justify-between h-full mx-auto w-full">
-                <Link href={"/"} className="flex items-center justify-start space-x-2"><div className="w-6 h-6 bg-black rounded-full"></div>
+                <Link href={"/"} className="flex items-center justify-start space-x-2">
+                    <Image src={"/supraswap.png"} width={40} height={40} alt="uspra" />
                     <h1>Supra</h1>
                 </Link>
                 {isConnected ? <div className="flex items-center gap-4">
+                    <Link href={"/swap"}>Mint</Link>
+                    <Link href={"/swap"}>Faucet</Link>
                     <HistoryModal />
                     <UserProfileNav />
                 </div> : <ConnectWalletButton />}
