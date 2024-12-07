4fb733918bf25992539162273a1a8dd5f209f0d0
KetanKudikyal <krotusppp@gmail.com>
2024-12-08 03:49:11 +0530

    feat: ✨ update mint interface and icons for a better user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
+'use client'
 import { ICONS } from "@/components/icons"
-import SwapInterface from "@/components/swap-interface"
+import MintInterface from "@/components/mint-interface"
 
 export default function Home() {
     return <main className="h-screen md:h-[95%] w-full flex flex-col justify-center items-center">
         <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
             <h1 className="text-2xl md:text-5xl font-bold tracking-tight mb-6">
-                Swap Any Token,
-                <br />
-                <div className="flex mx-auto w-max items-center gap-6">
+                <div className="flex items-center justify-start">
+                    Mint&nbsp;<ICONS.usdcRed />&nbsp;USDS&nbsp;Anytime
+                </div>
+                <div className="flex mx-auto w-max items-center mt-4 gap-6">
                     <span className="text-primary">On</span>
                     <ICONS.supraWithText />
                 </div>
             </h1>
         </div>
         <div className="max-w-md w-full">
-            <SwapInterface />
+            <MintInterface />
         </div>
     </main>
 }
